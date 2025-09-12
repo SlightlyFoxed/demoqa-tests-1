be97427bcf6cb07e9c2f033f3c193cbb36501e9d
Ilya Kozlov <catun3@gmail.com>
2025-09-12 07:16:14 +0300

    added allure_notification

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -24,7 +24,7 @@ public class TestBase {
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 10000;
         //Configuration.holdBrowserOpen = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String,Object>of(
                 "enableVNC", true,
